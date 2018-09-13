f9c80581c614bb260a5536b03d37989b43642d02
Melanie Rosado <MelanieRosado@148-100-138-123.FoxNet.marist.edu>
2018-09-13 15:19:45 -0400

    created a main function and a function call statement.

--- madlib.py
+++ madlib.py
@@ -1,10 +1,19 @@
-def prompt():
+def promptForWords():
+    global n,v,a,p
     n = input("Enter a noun: ")
     v = input("Enter a verb: ")
     a = input("Enter an adjective: ")
     p = input("Enter a place: ")
 
-    print("Take your", a , n , "and" , v , "it at the" ,p)    
-prompt()
+def makeAndPrintSentence():
+    print("Take your", a , n , "and" , v , "it at the" ,p)
+
+def main():
+    promptForWords()
+    makeAndPrintSentence()
+
+main()
+
+    
 
 

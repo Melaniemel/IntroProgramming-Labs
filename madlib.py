12e36d306cb8170f37ae6c07bd3c690b38827874
Melanie Rosado <MelanieRosado@148-100-138-123.FoxNet.marist.edu>
2018-09-13 14:45:57 -0400

    Solution to Mad Libs exercise.

---
+++ madlib.py
@@ -0,0 +1,10 @@
+def prompt():
+    n = input("Enter a noun: ")
+    v = input("Enter a verb: ")
+    a = input("Enter an adjective: ")
+    p = input("Enter a place: ")
+
+    print("Take your", a , n , "and" , v , "it at the" ,p)    
+prompt()
+
+
